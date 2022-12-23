6d79a7a5046969290b2523b000127cb14b982790
BigDany1792 <dany1792dota@gmail.com>
2022-12-22 20:25:30 -0500

    FEAT: The structure where the service ids are stored was changed to make it dynamic

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,26 +1,28 @@
-$(window).on('load', function() {
+$(window).on('load', function () {
   const showAmenities = {};
-  $('.popover input').change(function(event){
-    const evento = event.target
-    const nameAmenitie = evento.getAttribute('data-name')
-    const idAmenitie = evento.getAttribute('data-id')
 
-    if (evento.checked){
-      showAmenities[idAmenitie] = nameAmenitie
-    } else{
-      const idx = showAmenities.indexOf(nameAmenitie)
-      showAmenities.splice(idx, 1)
-      console.log("me desmarco")
+  $('.popover input').change(function (event) {
+    const evento = event.target;
+    const idAmenitie = evento.getAttribute('data-id');
+    const nameAmenitie = evento.getAttribute('data-name');
+
+    if (evento.checked) {
+      showAmenities[idAmenitie] = nameAmenitie;
+    } else {
+      delete showAmenities[idAmenitie];
     }
-    
-    if (showAmenities.length > 0){
-      const concatAmenities = showAmenities.join(', ')
-      if (concatAmenities.length > 40){
-        $('.amenities h4').text(concatAmenities.substring(0, 40 -3) + "...")
+
+    const listAmenities = Object.values(showAmenities);
+
+    if (listAmenities.length > 0) {
+      const concatAmenities = listAmenities.join(', ');
+      if (concatAmenities.length > 40) {
+        $('.amenities h4').text(concatAmenities.substring(0, 40 - 3) + '...');
       } else {
-        $('.amenities h4').text(concatAmenities)
+        $('.amenities h4').text(concatAmenities);
       }
+    } else {
+      $('.amenities h4').html('&nbsp;');
     }
-    
-    console.log(showAmenities)
-});})
+  });
+});
