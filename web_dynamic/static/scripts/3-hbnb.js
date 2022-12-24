ca30f851f890b2015fe831fda43894ed57af8d5f
gustavofranco26 <gustavo260123@gmail.com>
2022-12-24 00:27:40 -0500

    Feat: Add the first part of script that send a post request and return the sections places through frontend.

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -39,5 +39,34 @@ $(window).on('load', function () {
     }
   });
 
-
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    type: 'POST',
+    ContentType: 'application/json',
+    data: '{}',
+    sucess, function(data) {
+      let j;
+      for (j of data) {
+        $('section.places').append(
+        <article>
+          <div class="title_box">
+            <h2>${j.name}</h2>
+            <div class="price_by_night">
+              $${j.price_by_night}
+            </div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${j.max_guest} Guest${j.max_guest != 1}s${endif}</div>
+            <div class="number_rooms">${j.number_rooms} Bedroom${j.number_rooms != 1}s${endif}</div>
+            <div class="number_bathrooms">${j.number_bathrooms} Bathroom${j.number_bathrooms != 1}s${endif}</div>
+          </div>
+          <div class="user">
+            <b>Owner:</b> ${j.user.first_name} ${j.user.last_name}
+          </div>
+          <div class="description">
+            ${j.description | safe}
+          </div>
+       </article>)}
+    }
+  })
 });
